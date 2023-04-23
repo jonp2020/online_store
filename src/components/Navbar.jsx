80696045ce68bbf153a72b4bf88cdfd97018dc08
jonp2020 <jon.parnham@gmail.com>
2023-04-23 14:07:56 +0100

    Add tailwind and navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <section>
+        <ul className="flex justify-around mt-4">
+          <li>Home</li>
+          <li>Womens</li>
+          <li>Mens</li>
+          <li>Kids</li>
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default Navbar;
