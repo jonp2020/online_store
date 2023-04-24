66104d619581d2a61fbd62ac8c3b387212f417f8
jonp2020 <jon.parnham@gmail.com>
2023-04-24 21:23:02 +0100

    Add home hero images and links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import heroImage from "../assets/images/walking-large.jpg";
+
+// src/assets/images/walking-large.jpg
+
 const Home = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <>
+      <h2>Home page</h2>
+      <img src={heroImage} alt="Girl in a jacket" width="" height="" />
+      <div>
+        <h1>
+          <Link to="womens">Shop Womens</Link>
+        </h1>
+      </div>
+      <div>
+        <Link to="mens">
+          {" "}
+          <h1>Shop Mens</h1>
+        </Link>
+      </div>
+      <div>
+        <Link to="kids">
+          {" "}
+          <h1>Shop Kids</h1>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Home;
