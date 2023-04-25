dcd4800113c0f1445efb585a9fc6a9812dab8f8d
jonp2020 <jon.parnham@gmail.com>
2023-04-25 23:04:41 +0100

    Add shoe car

--- src/components/Mens.jsx
+++ src/components/Mens.jsx
@@ -1,5 +1,22 @@
+import Shoe from "./Shoe";
+
 const Mens = () => {
-  return <h2>Mens Page</h2>;
+  return (
+    <section>
+      <h2>Mens</h2>
+      <main class="flex flex-wrap">
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+        <Shoe />
+      </main>
+    </section>
+  );
 };
 
 export default Mens;
---
+++ src/components/Shoe.jsx
@@ -0,0 +1,14 @@
+const Shoe = () => {
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2 p-2">
+      <img
+        class=""
+        src="https://image.goat.com/375/attachments/product_template_pictures/images/008/654/831/original/31342_00.png.png"
+      />
+      <h4 class="text-center">Nike Air</h4>
+      <h5 class="text-center">£80</h5>
+    </div>
+  );
+};
+
+export default Shoe;
