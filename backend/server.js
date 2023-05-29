03a7476c6eb5e6a59f46765d0fb5a52260b85994
jonp2020 <jon.parnham@gmail.com>
2023-05-29 15:33:43 +0100

    Add mongoose db config to connect to mongoDB atlas db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const app = require("./app.js");
 const dotenv = require("dotenv").config();
-
+const connectDB = require("./config/db.js");
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
