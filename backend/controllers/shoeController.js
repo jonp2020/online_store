7e7f6a3710ab2ff036a51022a2baf50561ce841f
jonp2020 <jon.parnham@gmail.com>
2023-05-29 15:48:47 +0100

    Add mongoose query to get all shoe data

--- backend/controllers/shoeController.js
+++ backend/controllers/shoeController.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const Shoe = require("../models/shoeModel");
 
 const getAllShoes = asyncHandler(async (req, res) => {
-  const shoes = { shoes: "shoes example 1" };
-
-  res.status(200).json(shoes);
+  try {
+    const shoes = await Shoe.find();
+    res.status(200).json(shoes);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = {
